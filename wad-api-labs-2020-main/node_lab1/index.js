aa3a7a85c282bcbb722d08261fc72f5eddd834ca
Emma Murphy <20085230@mail.wit.ie>
2020-12-07 18:34:17 +0000

    node-lab1,  simple server

---
+++ wad-api-labs-2020-main/node_lab1/index.js
@@ -0,0 +1,17 @@
+// Load the http module to create an http server.
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = 8080;
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello  WAd2!');
+});
+
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log(`Server running at ${port}`);
